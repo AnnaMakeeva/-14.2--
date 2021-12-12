51d7276a31d6c3734aa3807b890e263d258492ed
Anna <anna.vm@mail.ru>
2021-12-12 13:07:29 +0300

    Добавьте файлы проекта.

---
+++ Задание 14.2-отправка на проверку/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_14._2_отправка_на_проверку
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Персик");
+            dog.ShowInfo();
+            Cat cat = new Cat("Снегирка");
+            cat.ShowInfo();
+
+            Console.ReadKey();
+        }
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+
+            public abstract void Say();
+
+            public virtual void ShowInfo()
+            {
+                Console.WriteLine(" {0} ", Name);
+                Say();
+            }
+            public Animal(string name)
+            {
+                Name = name;
+            }
+        }
+        class Dog : Animal
+        {
+
+            string name;
+            public override string Name      //переопределяем свойство
+            {
+                get //срабатывает при попытки  получить значение
+                {
+                    return name;
+                }
+                set //срабатывает при попытки установить значение
+                {
+                    name = value;
+                }
+            }
+                 public Dog(string name) : base(name)
+                 {
+
+                 }
+
+                 public override void Say()
+                 {
+                   Console.WriteLine("\"Гав\"");
+
+                 }
+           
+        }
+        class Cat : Animal
+        {
+
+            string name;
+            public override string Name      //переопределяем свойство
+            {
+                get //срабатывает при попытки  получить значение
+                {
+                    return name;
+                }
+                set //срабатывает при попытки установить значение
+                {
+                    name = value;
+                }
+            }
+            public Cat(string name) : base(name)
+            {
+
+            }
+
+            public override void Say()
+            {
+                Console.WriteLine("\"Мяу\"");
+
+            }
+
+        }
+
+
+
+
+
+    }
+}
